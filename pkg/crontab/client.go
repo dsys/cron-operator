c9723ffb5ff4134713ea054d770a84206f32b70c
Alex Kern <alex@kern.io>
2017-07-26 23:18:14 -0700

    An attempt at Kube vendoring

--- pkg/crontab/client.go
+++ pkg/crontab/client.go
@@ -4,7 +4,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/pkg/api"
+	// "k8s.io/client-go/pkg/api"
 	"k8s.io/client-go/rest"
 )
 
@@ -13,14 +13,14 @@ type ChargebackInterface interface {
 	CronTabGetter
 }
 
-func NewForConfig(c *rest.Config) (*ChargebackClient, error) {
+func NewForConfig(c *rest.Config) (*CronOperatorClient, error) {
 	newC := *c
 	newC.GroupVersion = &schema.GroupVersion{
 		Group:   Group,
 		Version: Version,
 	}
 	newC.APIPath = "/apis"
-	newC.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: api.Codecs}
+	// newC.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: api.Codecs}
 
 	client, err := rest.RESTClientFor(&newC)
 	if err != nil {
@@ -32,14 +32,14 @@ func NewForConfig(c *rest.Config) (*ChargebackClient, error) {
 		return nil, err
 	}
 
-	return &ChargebackClient{client, dynamicClient}, nil
+	return &CronOperatorClient{client, dynamicClient}, nil
 }
 
-type ChargebackClient struct {
+type CronOperatorClient struct {
 	restClient    rest.Interface
 	dynamicClient *dynamic.Client
 }
 
-func (c *ChargebackClient) Reports(namespace string) CronTabInterface {
+func (c *CronOperatorClient) Reports(namespace string) CronTabInterface {
 	return newCronTabs(c.restClient, c.dynamicClient, namespace)
 }
